19715be5a792fb3f9333183634a02df7d06900dd
resjudicoda <adam.j.reis@gmail.com>
2023-03-19 11:25:25 -0700

    changed styling

--- components/elements/profileBar/ProfileBar.tsx
+++ components/elements/profileBar/ProfileBar.tsx
@@ -7,7 +7,7 @@ import {
     Progress
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router';
-import NextLink from 'next/link';
+import Link from 'next/link';
 import '@rainbow-me/rainbowkit/styles.css';
 
 interface ProfileBarProps {
@@ -21,42 +21,44 @@ export const ProfileBar = ({ bgColor, headingColor, customPadding, radius }: Pro
     const router = useRouter()
 
     return (
-      <Box bgColor={bgColor} rounded={radius} padding={customPadding}>
-        <Flex>
-          <Flex flexBasis={['20%','15%']} alignItems='center'>
-            <Image alt='user-pfp' boxSize={58} rounded={30} bgColor='gray' justifyContent='center' src='/css-gradient.png'></Image>
-          </Flex>
-            <Flex justify={['space-evenly']} flexBasis={'85%'}>
-                <Flex flexDir={'column'} align={['flex-start', 'center']} flexBasis={['33.33%']}>
-                  <Heading size={['sm','md']} color={headingColor}>Project</Heading>
-                  <Flex  grow={'1'}>
-                    <Text color={headingColor} fontSize={['12px', '16px']} textAlign={['left', 'center']}>Lazy Chicken Society</Text>
+      <Link href="/funding">
+        <Box bgColor={bgColor} rounded={radius} padding={customPadding}>
+          <Flex>
+            <Flex flexBasis={['20%','15%']} alignItems='center'>
+              <Image alt='user-pfp' boxSize={58} rounded={30} bgColor='gray' justifyContent='center' src='/css-gradient.png'></Image>
+            </Flex>
+              <Flex justify={['space-evenly']} flexBasis={'85%'}>
+                  <Flex flexDir={'column'} align={['flex-start', 'center']} flexBasis={['33.33%']}>
+                    <Heading size={['sm','md']} color={headingColor}>Project</Heading>
+                    <Flex  grow={'1'}>
+                      <Text color={headingColor} fontSize={['12px', '16px']} textAlign={['left', 'center']}>Lazy Chicken Society</Text>
+                    </Flex>
                   </Flex>
-                </Flex>
-                <Flex flexDir={'column'} align={['flex-start', 'center']} flexBasis={['33.33%']}>
-                  <Heading size={['sm','md']} color={headingColor}>Seeking</Heading>
-                  <Flex grow={'1'}>
-                    <Text color={headingColor} fontSize={['12px', '16px']} textAlign={['left', 'center']}>$500 USDC</Text>
+                  <Flex flexDir={'column'} align={['flex-start', 'center']} flexBasis={['33.33%']}>
+                    <Heading size={['sm','md']} color={headingColor}>Seeking</Heading>
+                    <Flex grow={'1'}>
+                      <Text color={headingColor} fontSize={['12px', '16px']} textAlign={['left', 'center']}>$500 USDC</Text>
+                    </Flex>
                   </Flex>
-                </Flex>
-                <Flex  flexDir={'column'} align={['flex-start', 'center']} flexBasis={['33.33%']}>
-                  <Heading size={['sm','md']} color={headingColor}>Funders</Heading>
-                  <Flex pos={'relative'}>
-                    <Flex pos={'absolute'} zIndex={[3, 2]} left={['8', '1']} boxSize={50} rounded={30} justifyContent='center' alignItems='center' bgImage='/blue-gradient.webp'></Flex>
-                    <Flex pos={'absolute'} zIndex={[2, 1]} left={['4', '-5']} boxSize={50} rounded={30} bgColor='green' justifyContent='center' alignItems='center'></Flex>
-                    <Flex pos={'absolute'} zIndex={0} left={[null, '-10']} boxSize={50} rounded={30} bgColor='gray' justifyContent='center' alignItems='center'></Flex>
-                  </Flex>
-                </Flex>   
+                  <Flex  flexDir={'column'} align={['flex-start', 'center']} flexBasis={['33.33%']}>
+                    <Heading size={['sm','md']} color={headingColor}>Funders</Heading>
+                    <Flex pos={'relative'}>
+                      <Flex pos={'absolute'} zIndex={[3, 2]} left={['8', '1']} boxSize={50} rounded={30} justifyContent='center' alignItems='center' bgImage='/blue-gradient.webp'></Flex>
+                      <Flex pos={'absolute'} zIndex={[2, 1]} left={['4', '-5']} boxSize={50} rounded={30} bgColor='green' justifyContent='center' alignItems='center'></Flex>
+                      <Flex pos={'absolute'} zIndex={0} left={[null, '-10']} boxSize={50} rounded={30} bgColor='gray' justifyContent='center' alignItems='center'></Flex>
+                    </Flex>
+                  </Flex>   
+            </Flex>
           </Flex>
-        </Flex>
-        <Box mt={6}>
-          <Flex justifyContent={'space-between'}>
-            <Heading color={headingColor} mb={2} size={'sm'}>Sold: 20</Heading>
-            <Heading color={headingColor} mb={2} size={'sm'}>Bought: 20</Heading>
-          </Flex>
-          <Progress value={20} size='sm' colorScheme='pink' rounded={1}/>
+          <Box mt={6}>
+            <Flex justifyContent={'space-between'}>
+              <Heading color={headingColor} mb={2} size={'sm'}>Sold: 20</Heading>
+              <Heading color={headingColor} mb={2} size={'sm'}>Bought: 20</Heading>
+            </Flex>
+            <Progress value={20} size='sm' colorScheme='pink' rounded={1}/>
+          </Box>
         </Box>
-      </Box>
+      </Link>
     )
 
 }
