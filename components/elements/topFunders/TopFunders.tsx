d0a133771610d8a51fabc61128486f3ab51fdd0d
Anthony Urbina <aurbinava002@gmail.com>
2023-03-18 17:56:53 -0700

    create topFunders component

---
+++ components/elements/topFunders/TopFunders.tsx
@@ -0,0 +1,80 @@
+import {
+    Box,
+    Flex,
+    Link,
+    Button,
+    Image,
+    HStack,
+    LinkBox,
+    LinkOverlay,
+    IconButton,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Text,
+    Show,
+    Hide,
+    Heading,
+    Accordion,
+    AccordionButton,
+    AccordionItem,
+    AccordionIcon,
+    AccordionPanel
+} from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import { useRouter } from 'next/router';
+import NextLink from 'next/link';
+import '@rainbow-me/rainbowkit/styles.css';
+import { ProfileBar } from '../profileBar';
+
+import {
+    getDefaultWallets,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+export const TopFunders = () => {
+    const router = useRouter()
+
+    return (
+        <>        
+        <Accordion allowToggle>
+            <AccordionItem color='white'>
+                <h2>
+                <AccordionButton bgColor='#212C3B' border='1px solid #4E5D73' rounded='.25rem'>
+                    <Box as="span" flex='1' textAlign='left' fontSize={21} fontWeight='semibold'>
+                    Top Funders
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                <ProfileBar />
+                </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+                <h2>
+                <AccordionButton>
+                    <Box as="span" flex='1' textAlign='left'>
+                    Section 2 title
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+                </AccordionPanel>
+            </AccordionItem>
+        </Accordion>
+        </>
+    )
+
+}
